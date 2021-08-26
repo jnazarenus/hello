5818625c17fb45a08928f39eca9448920338ac4c
Josh Nazarenus <jnazarenus2@huskers.unl.edu>
2021-08-26 18:06:00 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Josh Nazarenus
+ * Date:    2020
+ *
+ * A simple hello world program
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+
+    printf ("Josh Nazarenus\n");
+    printf ("Computer Science\n");
+
+    return 0;
+
+ }
